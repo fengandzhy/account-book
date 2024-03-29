3972e30d6a1369726bf9556157e73a62fac91477
fengandzhy <fengandzhy@gmail.com>
2024-03-30 10:13:40 +1300

    accounting-book create list page

--- accounting-book/routes/index.js
+++ accounting-book/routes/index.js
@@ -1,9 +1,15 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+/* accounting book list page. */
+router.get('/account', function(req, res, next) {
+  res.render('list', { title: 'Express' });
+
+});
+
+router.get('/account/create', function(req, res, next) {
+  // res.render('index', { title: 'Express' });
+  res.send('添加记录');
 });
 
 module.exports = router;
