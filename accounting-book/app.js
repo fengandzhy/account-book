e00d1392e63cb0cb10658ae53ba576c96f425946
fengandzhy <fengandzhy@gmail.com>
2024-04-01 10:40:03 +1300

    accounting-book login functionality

--- accounting-book/app.js
+++ accounting-book/app.js
@@ -9,6 +9,9 @@ const usersRouter = require('./routes/users');
 const accountRouter = require('./routes/account-api');
 const authRouter = require('./routes/auth');
 
+const session = require("express-session");
+const MongoStore = require('connect-mongo');
+
 const app = express();
 
 // view engine setup
@@ -26,7 +29,21 @@ app.use('/', indexRouter);
 app.use('/api', accountRouter); // 在访问account 时，必须要在前面加上api
 app.use('/', authRouter);
 
+const {DB_HOST, DB_PORT, DB_NAME} = require('./config/config.js');
 
+app.use(session({
+  name: 'sid',   //设置cookie的name，默认值是：connect.sid
+  secret: 'atguigu', //参与加密的字符串（又称签名）  加盐
+  saveUninitialized: false, //是否为每次请求都设置一个cookie用来存储session的id
+  resave: true,  //是否在每次请求时重新保存session  20 分钟    4:00  4:20
+  store: MongoStore.create({
+    mongoUrl: `mongodb://${DB_HOST}:${DB_PORT}/${DB_NAME}` //数据库的连接配置
+  }),
+  cookie: {
+    httpOnly: true, // 开启后前端无法通过 JS 操作
+    maxAge: 1000 * 60 * 60 * 24 * 7 // 这一条 是控制 sessionID 的过期时间的！！！
+  },
+}))
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
--- accounting-book/routes/auth.js
+++ accounting-book/routes/auth.js
@@ -36,36 +36,33 @@ router.get('/login', (req, res) => {
 });
 //
 // //登录操作
-// router.post('/login', (req, res) => {
-//     //获取用户名和密码
-//     let {username, password} = req.body;
-//     //查询数据库
-//     UserModel.findOne({username: username, password: md5(password)}, (err, data) => {
-//         //判断
-//         if(err){
-//             res.status(500).send('登录, 请稍后再试~~');
-//             return
-//         }
-//         //判断 data
-//         if(!data){
-//             return res.send('账号或密码错误~~');
-//         }
-//         //写入session
-//         req.session.username = data.username;
-//         req.session._id = data._id;
-//
-//         //登录成功响应
-//         res.render('success', {msg: '登录成功', url: '/account'});
-//     })
-//
-// });
-//
-// //退出登录
-// router.post('/logout', (req, res) => {
-//     //销毁 session
-//     req.session.destroy(() => {
-//         res.render('success', {msg: '退出成功', url: '/login'});
-//     })
-// });
+router.post('/login', async (req, res) => {
+    //获取用户名和密码
+    const {username, password} = req.body;
+    try {
+        const user = await userModel.findOne({username: username, password: md5(password)});
+        if(!user){
+            return res.send('账号或密码错误~~');
+        }
+        //写入session
+        req.session.username = user.username;
+        req.session._id = user._id;
+        //登录成功响应
+        res.render('success', {msg: '添加成功', url: '/account'});
+    } catch (err) {
+        console.log(err);
+        // 判断是否有错误
+        return res.status(500).send('登录, 请稍后再试~~');
+    }
+
+});
+
+//退出登录
+router.post('/logout', (req, res) => {
+    //销毁 session
+    req.session.destroy(() => {
+        res.render('success', {msg: '退出成功', url: '/login'});
+    })
+});
 
 module.exports = router;
